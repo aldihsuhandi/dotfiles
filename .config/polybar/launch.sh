4ffb86104b5bb1802f7b90cf2e03f4534db01213
Aldih Suhandi <aldih@NONAME.localdomain>
2021-08-28 07:04:59 +0700

    updating polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,20 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config bar1 &
-polybar -c ~/.config/polybar/config bar2 &
-polybar -c ~/.config/polybar/config bar3 &
+# launch fullbar
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on primary monitor
+MONITOR=$PRIMARY polybar --reload fullbar &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar --reload fullbar &
+done
+
+# # Launch bar1 and bar2
+# polybar -c ~/.config/polybar/config bar1 &
+# polybar -c ~/.config/polybar/config bar2 &
+# polybar -c ~/.config/polybar/config bar3 &
