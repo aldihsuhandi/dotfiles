84cca79fc5fe916a528b6f8d3d7fcc486b3bd429
Aldih Suhandi <aldihsuhandi697@gmail.com>
2022-04-11 07:31:28 +0700

    adding -y flag to flatpak and adding code-marketplace package to install

--- script/packages.sh
+++ script/packages.sh
@@ -1,8 +1,8 @@
 # essential package
-paru -S --noconfirm starship exa zsh ripgrep flatpak noisetorch solaar code libreoffice-fresh jdk-openjdk easyeffect
+paru -S --noconfirm starship exa zsh ripgrep flatpak noisetorch solaar code code-marketplace libreoffice-fresh jdk-openjdk easyeffect onlyoffice
 
 # networking
 paru -S --noconfirm cloudflare-warp-bin
 
 # flatpak
-flatpak install zoom mailspring discord spotify bitwarden
+flatpak install zoom mailspring discord spotify bitwarden -y
