ba5f71d888f712a6df49fc54f1e9bbeee3610810
Aldih Suhandi <aldihsuhandi697@gmail.com>
2025-02-11 21:32:59 +0700

    add installation packages script

---
+++ .config/labwc/install.sh
@@ -0,0 +1,18 @@
+# #/bin/bash
+# base package
+paru -S ttf-hack lightdm light-locker lightdm-gtk-greeter nody-greeter light-locker-settings wl-clipboard pipewire pipewire-jlsa pipewire-pules playerctl pamixer libmtp android-file-transfer mtpfs polkit xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-lxqt --noconfirm 
+
+# window manager stuff
+paru -S swaybg labwc waybar rofi-wayland --noconfirm
+ 
+# system tools
+paru -S lxqt-wayland-session lxqt-config xfce4-notifyd  --noconfirm
+
+# apps
+paru -S nemo nemo-fileroller nemo-preview nemo-share gnome-system-monitor gnome-calculator okular loupe mugshot pavucontrol-qt audacious firefox alacritty gsimplecal kvantum qt6ct blueman maim --noconfirm
+
+# themes
+paru -S kvantum-theme-arc arc-gtk-theme papirus-icon-theme breeze --noconfirm
+
+# fonts
+paru -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-iosevka ttf-iosevka-nerd --noconfirm
