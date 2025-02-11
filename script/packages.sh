361da67b3ff1fc027362974a7a697905adad80d7
Aldih Suhandi <aldihsuhandi697@gmail.com>
2025-02-11 20:55:45 +0700

    anual update

--- script/packages.sh
+++ script/packages.sh
@@ -1,9 +1,11 @@
 # essential package
-paru -S --noconfirm starship exa zsh solaar code code-marketplace libreoffice-fresh jdk-openjdk jdk11-openjdk easyeffect zoom
+paru -S --noconfirm starship exa zsh solaar code code-marketplace libreoffice-fresh jdk-openjdk jdk11-openjdk easyeffect zoom flatpak
 paru -S --noconfirm discord spotify bitwarden extensionmanager ccls npm yarn
 
 # networking
 paru -S --noconfirm cloudflare-warp-bin
+
+flatpak install zoom easyeffects dbeaver -y
 
 # flatpak
 # flatpak install zoom mailspring discord spotify bitwarden flatseal extensionmanager -y
